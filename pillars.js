75f85bc40a9941c89d88b76952211581c6336c79
somersaultjump <somersaultjump@afakeemailaddress.com>
2018-02-10 07:53:42 -0700

    fixing pillar rotation

--- pillars.js
+++ pillars.js
@@ -8,58 +8,79 @@ function pillars() {
 	fill(40,40,250,200);
 	
 
-	if (hour() === 0 || 
-		hour() === 6 ||
-		hour() === 12 ||
-		hour() === 18) {
-		rotateZ(PI);
-	} else if (hour() === 1 || 
-		hour() === 7 ||
-		hour() === 13 ||
-		hour() === 19) {
-		rotateZ(5*PI/6);
-	} else if (hour() === 2 || 
-		hour() === 8 ||
-		hour() === 14 ||
-		hour() === 20) {
-		rotateZ(2*PI/3);
-	} else if (hour() === 3 || 
-		hour() === 9 ||
-		hour() === 15 ||
-		hour() === 21) {
-		rotateZ(PI*1.5);
-	} else if (hour() === 4 || 
-		hour() === 10 ||
-		hour() === 16 ||
-		hour() === 22) {
-		rotateZ(PI/1.5);
-	} else if (hour() === 5 || 
-		hour() === 11 ||
-		hour() === 17 ||
-		hour() === 23) {
-		rotateZ((11*PI)/6);
-	}
+// event callback is a way better way to do the time.
 
 	/*
-	PI = 6
-	5*PI/6 = 5
-	2*PI/3 = 4
-	PI/2 = 3
-	PI/3 = 2
-	PI/6 = 1
-	PI*2 = 12
-	11*PI/6 = 11
-	5*PI/3 = 10
-	3*PI/2 = 9
-	4*PI/3 = 8
-	7*PI/6 =7
-
+	PI*2     = 12
+	PI/6     = 1
+	PI/3     = 2
+	PI/2     = 3
+	2*PI/3   = 4
+	5*PI/6   = 5
+	PI       = 6
+	7*PI/6   = 7
+	4*PI/3   = 8
+	3*PI/2   = 9
+	5*PI/3   = 10
+	11*PI/6  = 11
 	*/
+
+switch(hour()) {
+	case 0:
+	case 12:
+		rotateZ(PI*2);
+		break;
+	case 1:
+	case 13:
+		rotateZ(PI/6);
+		break;
+	case 2:
+	case 14:
+		rotateZ(PI/3);
+		break;
+	case 3:
+	case 15:
+		rotateZ(PI/2);
+		break;
+	case 4:
+	case 16:
+		rotateZ(2*PI/3);
+		break;
+	case 5:
+	case 17:
+		rotateZ(5*PI/6);
+		break;
+	case 6:
+	case 18:
+		rotateZ(PI);
+		break;
+	case 7:
+	case 19:
+		rotateZ(7*PI/6);
+		break;
+	case 8:
+	case 20:
+		rotateZ(4*PI/3);
+		break;
+	case 9:
+	case 21:
+		rotateZ(3*PI/2);
+		break;
+	case 10:
+	case 22:
+		rotateZ(5*PI/3);
+		break;
+	case 11:
+	case 23:
+		rotateZ(11*PI/6);
+		break;
+} 
+
 
 	console.log(hour());
 
 	// rotateX(0);
-	rotateY(millis()/2700);
+	rotateY(second()*60/360);
 	// rotateZ(0);
 
 	// translate(0,0);
